b08deddb21d74b84366bb73cecb0b1f58a60c6ce
nicolas.sassi <nicolas.sassi@b2wdigital.com>
2020-07-27 15:14:46 -0300

    add simple exe example

--- main.go
+++ main.go
@@ -1,5 +1,43 @@
 package kinestesia
 
-func main() {
+import (
+	"context"
+	"github.com/nicolasassi/kinestesia/kinesis"
+	"github.com/nicolasassi/kinestesia/receivers"
+	"log"
+	"os"
+)
 
+func main() {
+	ctx := context.Background()
+	log.Println("starting execution...")
+	cctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	client, err := kinesis.NewClient(cctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s, err := kinesis.NewStreamer(cctx, os.Getenv("STREAM_NAME"), client)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("starting pubsub receiver...")
+	pbs, err := receivers.NewPubSubClient(cctx, os.Getenv("PROJECT_ID"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("selected environmt %s", os.Getenv("ENVIRONMENT"))
+	var pbsTopic string
+	switch os.Getenv("ENVIRONMENT") {
+	case "PRD":
+		pbsTopic = os.Getenv("PUBSUB_TOPIC")
+	default:
+		pbsTopic = os.Getenv("PUBSUB_TOPIC_HML")
+	}
+	log.Printf("streming data to pusub topic: %s", pbsTopic)
+	pbs.AddTopics(pbsTopic)
+	log.Printf("streaming data from %s",	os.Getenv("STREAM_NAME"))
+	if err := s.Stream(cctx, pbs); err != nil {
+		log.Fatal(err)
+	}
 }
