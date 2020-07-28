dc43949f7e7a0281535b11f9e53f625e0e287772
nicolas.sassi <nicolas.sassi@b2wdigital.com>
2020-07-28 15:35:10 -0300

    test filtering

--- receivers/pubsub_test.go
+++ receivers/pubsub_test.go
@@ -33,10 +33,10 @@ func TestClient_Translate(t *testing.T) {
 		wantErr   bool
 	}{
 		{"default", fields{
-			client:     nil,
-			topics:     nil,
-			stream:     nil,
-			errors:     nil,
+			client: nil,
+			topics: nil,
+			stream: nil,
+			errors: nil,
 		},
 			args{b: func() []byte {
 				values := map[string]interface{}{
@@ -61,10 +61,10 @@ func TestClient_Translate(t *testing.T) {
 			},
 			[]byte(`{"aa":"a","b":"b","cd":1}`), false},
 		{"keyInSliceOfMap", fields{
-			client:     nil,
-			topics:     nil,
-			stream:     nil,
-			errors:     nil,
+			client: nil,
+			topics: nil,
+			stream: nil,
+			errors: nil,
 		},
 			args{b: func() []byte {
 				values := map[string]interface{}{
@@ -94,10 +94,10 @@ func TestClient_Translate(t *testing.T) {
 			},
 			[]byte(`{"aa":"a","b":"b","chi":1}`), false},
 		{"valueInSlice", fields{
-			client:     nil,
-			topics:     nil,
-			stream:     nil,
-			errors:     nil,
+			client: nil,
+			topics: nil,
+			stream: nil,
+			errors: nil,
 		},
 			args{b: func() []byte {
 				values := map[string]interface{}{
@@ -109,7 +109,7 @@ func TestClient_Translate(t *testing.T) {
 						{"f": 3},
 						{"h": map[string]interface{}{
 							"i": 1,
-							"j": []int{1,2,3,4,5},
+							"j": []int{1, 2, 3, 4, 5},
 						}},
 					},
 				}
@@ -120,13 +120,13 @@ func TestClient_Translate(t *testing.T) {
 				return b
 			}(),
 			}, reference{
-			ref: map[string]string{
-				"a":     "aa",
-				"c.h.i": "chi",
-				"c.h.j.[1]": "j",
+				ref: map[string]string{
+					"a":         "aa",
+					"c.h.i":     "chi",
+					"c.h.j.[1]": "j",
+				},
+				sep: ".",
 			},
-			sep: ".",
-		},
 			[]byte(`{"aa":"a","b":"b","chi":1,"j":2}`), false},
 	}
 	for _, tt := range tests {
@@ -146,6 +146,63 @@ func TestClient_Translate(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Translate() got = %v, want %v", string(got), string(tt.want))
+			}
+		})
+	}
+}
+
+func TestClient_TranslateWithFilter(t *testing.T) {
+	type fields struct {
+		client     *pubsub.Client
+		name       string
+		topics     []string
+		translator *translator.Translator
+		stream     chan []byte
+		sent       chan struct{}
+		errors     chan error
+	}
+	type args struct {
+		b []byte
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    []byte
+		wantErr bool
+	}{
+		{"default", fields{
+			translator: func() *translator.Translator {
+				t := new(translator.Translator)
+				t.AddFilterRule("operation", "==", "INSERT")
+				return t
+			}(),
+		}, args{
+			func() []byte {
+				raw := `{"operation":"UPDATE","type":"FHNS","id":"2983736282937","version":"1","sentAt":"2020-07-27T12:01:01.833-03:00","payload":{"id":"9832987392873","condition":"NEW","entity":"9809809","structure":{"dep":"09090","sec":"0909565","subclass":"34434","class":"6565"},"quantity":99,"updatedAt":"2020-07-27T12:01:01.832-03:00"}}`
+				return []byte(raw)
+			}()},
+			nil,
+			false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Client{
+				client:     tt.fields.client,
+				name:       tt.fields.name,
+				topics:     tt.fields.topics,
+				translator: tt.fields.translator,
+				stream:     tt.fields.stream,
+				sent:       tt.fields.sent,
+				errors:     tt.fields.errors,
+			}
+			got, err := c.Translate(tt.args.b)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Translate() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Translate() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
